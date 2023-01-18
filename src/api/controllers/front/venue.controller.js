b318bbc700911eca3cb5d5b0262a030f2932d509
Furqan <furqan72672@gmail.com>
2023-01-19 02:44:14 +0500

    minor tweaks

--- src/api/controllers/front/venue.controller.js
+++ src/api/controllers/front/venue.controller.js
@@ -1,4 +1,5 @@
 const Venue = require("../../models/venue.model")
+const Rating = require("../../models/rating.model")
 const Responses = require("../../utils/responses")
 
 class Controller{
@@ -7,6 +8,33 @@ class Controller{
     static async list(req,res,next){
         try{
             const venues=await Venue.find().populate({path: 'rating',populate: {path: 'user'} })
+            // venues.forEach((venue)=>{
+            //     const totalRating=await Rating.aggregate([
+            //         {
+            //             $group:{
+            //                 venue:"$venue",
+            //                 total:{$sum:"$stars"},
+            //                 count:{$sum:1}
+            //             }
+            //         }
+            //     ])
+            // })
+
+            // const totalRating=await Rating.aggregate([
+            //     {
+            //         $match:{
+            //             venue:'63bdb79d1e921f4852377f2b'
+            //         }
+            //     },
+            //     {
+            //         $group:{
+            //             total:{$sum:"$stars"},
+            //             count:{$sum:1}
+            //         }
+            //     }
+            // ])
+            // console.log(totalRating)
+            
             Responses.success(venues,200,'list retrieved',res)
         }
         catch(err){
